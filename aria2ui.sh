e58837cb4182b23c5db35a65e34085798780dc2b
flashbenny <flashbenny@users.noreply.github.com>
2018-04-03 00:13:03 +0800

    Update aria2ui.sh
    fix shell issue

--- aria2ui.sh
+++ aria2ui.sh
@@ -1,8 +1,8 @@
 #!bin/sh
-if [! -f /conf/aria2.conf];then
+if [ ! -f /conf/aria2.conf ];then
     echo "Error: Can't find Aria2 Config file."
 else
-	if [ ! -f /conf/aria2.session];then
+	if [ ! -f /conf/aria2.session ];then
 		touch /conf/aria2.session
 	fi
     darkhttpd /AriaNG/  --port 18800
